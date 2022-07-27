ca04aef23d771ab7340f7b96472283daa8be3e07
Nicola Milella <nicombk@gmail.com>
2022-07-27 09:42:54 +0200

    fix building assets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -34,6 +34,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // });
 
 // importamos nuestro archivo css 
-import '../css/app.css'
+// import '../css/app.css'
 import * as bootstrap from 'bootstrap'
 import * as logout from './logout'
