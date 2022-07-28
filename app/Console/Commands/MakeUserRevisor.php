f3de0fe2daab6ecff80969b575ddc11f372d9302
Nicola Milella <nicombk@gmail.com>
2022-07-28 12:15:40 +0200

    user3: make user revisor, migration and command

---
+++ app/Console/Commands/MakeUserRevisor.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class MakeUserRevisor extends Command
+{
+    /**
+    * The name and signature of the console command.
+    *
+    * @var string
+    */
+    protected $signature = 'rapido:makeUserRevisor';
+    // protected $signature = 'rapido:makeUserRevisor {email}'; alternativa
+    
+    /**
+    * The console command description.
+    *
+    * @var string
+    */
+    protected $description = 'Asigna el rol de revisor a un usuario';
+    
+    /**
+    * Execute the console command.
+    *
+    * @return int
+    */
+    public function handle()
+    {
+        
+        $email = $this->ask("Introducir el correo del usuario");
+        $user = User::where('email', $email)->first(); 
+        // $user = User::where('email', $this->argument('email'))->first(); alternativa
+
+        if(!$user){
+            $this->error("Usuario no encontrado");
+            return;
+        }
+        $user->is_revisor = true;
+        $user->save();
+        $this->info("El usuario $user->name ya es un revisor");
+    }
+}
