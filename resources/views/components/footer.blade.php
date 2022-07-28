2c9fc73f2bc4d2dbac626fb7eeba3e8c546732b6
Nicola Milella <nicombk@gmail.com>
2022-07-28 11:07:56 +0200

    basic footer style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,15 @@
-Hola
+<div class="text-bg-primary py-3 fixed-bottom">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                Powered by hackademy
+            </div>
+            <div class="col-12 col-md-6 text-end">
+                <a href="#" class="text-white text-decoration-none">Policy</a>
+                <a href="#" class="text-white text-decoration-none">Work with us</a>
+                <a href="#" class="text-white text-decoration-none">Facebook</a>
+                <a href="#" class="text-white text-decoration-none">Instagram</a>
+            </div>
+        </div>
+    </div>
+</div>
