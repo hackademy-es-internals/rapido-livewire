30817df494c996a51b7e640be629bf1aef445458
Nicola Milella <nicombk@gmail.com>
2022-08-03 18:51:52 +0200

    user4: translating using messages files

---
+++ lang/it/messages.php
@@ -0,0 +1,7 @@
+<?php
+ 
+// lang/it/messages.php
+ 
+return [
+    'welcome' => 'Benvenuto nella tua applicazione',
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Welcome to Rapido</h1>
+                <h1>{{__('messages.welcome')}}</h1>
             </div>
         </div>
         <div class="row">
